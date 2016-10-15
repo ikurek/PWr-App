5fe7089622b9b43b718ac279e1575660c8862422
ikurek <igorkurek96@gmail.com>
2016-10-15 03:29:51 +0200

    Gotowy popup dla newsfeed'u
    Newsfeed ma teraz popup z różnymi przydatnymi opcjami. Poprawiłem też trochę layout kart

--- app/src/main/java/layout/NewsFragment.java
+++ app/src/main/java/layout/NewsFragment.java
@@ -1,15 +1,24 @@
 package layout;
 
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.customtabs.CustomTabsIntent;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.PopupMenu;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -17,6 +26,7 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.ikurek.pwr.AsyncXMLParser;
+import com.ikurek.pwr.MainActivity;
 import com.ikurek.pwr.ParsedWebData;
 import com.ikurek.pwr.R;
 
@@ -30,6 +40,7 @@ public class NewsFragment extends Fragment {
     private AsyncXMLParser parser;
     private ListView listView;
     private SharedPreferences preferences;
+    private View viewForSnackbar;
 
 
     public NewsFragment() {
@@ -63,6 +74,8 @@ public class NewsFragment extends Fragment {
         parser = new AsyncXMLParser(this.getContext(), listView, progressBar);
         parser.execute();
 
+        viewForSnackbar = view;
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -88,7 +101,72 @@ public class NewsFragment extends Fragment {
         });
 
 
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View v, int position, long id) {
+
+                ParsedWebData singleData = NewsFragment.list.get(position);
+                final String url = singleData.getUrl();
+                final String title = singleData.getTitle();
+
+                PopupMenu popup = new PopupMenu(getContext(), v);
+                MenuInflater inflater = popup.getMenuInflater();
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        switch (item.getItemId()) {
+                            case R.id.menu_copy:
+
+                                ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                                ClipData clip = ClipData.newPlainText(title, url);
+                                clipboard.setPrimaryClip(clip);
+
+
+                                Snackbar snackbar = Snackbar.make(viewForSnackbar, "Skopiowano do schowka!", Snackbar.LENGTH_SHORT);
+
+                                snackbar.show();
+
+                                return true;
+
+                            case R.id.menu_share:
+
+                                Intent share = new Intent(android.content.Intent.ACTION_SEND);
+                                share.setType("text/plain");
+
+                                share.putExtra(Intent.EXTRA_SUBJECT, title);
+                                share.putExtra(Intent.EXTRA_TEXT, url);
+
+                                startActivity(Intent.createChooser(share, "Udostępnij"));
+
+                                return true;
+
+                            case R.id.menu_open_in_browser:
+
+                                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                                startActivity(browserIntent);
+
+                                return true;
+
+                            default:
+                                return false;
+                        }
+
+                    }
+                });
+
+                inflater.inflate(R.menu.newsfeed_long_click_popup_menu, popup.getMenu());
+                popup.show();
+
+
+                return true;
+            }
+        });
+
+
+
+
         return view;
     }
-
 }
